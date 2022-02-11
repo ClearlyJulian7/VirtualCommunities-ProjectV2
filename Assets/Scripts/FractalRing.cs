fca84be7cefa89da0448cbfe63c05a6898177b19
Daniel <danielrios3000@gmail.com>
2022-02-11 00:11:44 -0500

    Adding stuff

--- Assets/Scripts/FractalRing.cs
+++ Assets/Scripts/FractalRing.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fractal : MonoBehaviour
+public class FractalRing : MonoBehaviour
 {
     public static int count = 0;
     public static int ringer = 0;
     public float timer = 0;
 
-    public Fractal parent;
+    public FractalRing parent;
 
     [SerializeField]
     float objects = 150;
@@ -17,16 +17,20 @@ public class Fractal : MonoBehaviour
     [SerializeField]
     GameObject first;
 
+    public Vector3 pivot = new Vector3(0, -5, 0);
+
     // Start is called before the first frame update
     void Start()
     {
         if (this.transform.parent != null)
         {
-            parent = this.GetComponentInParent<Fractal>();
+            parent = this.GetComponentInParent<FractalRing>();
             // timer = parent.timer - 1.25f;
             timer = parent.timer - 1f;
         }
-        name = "Fractal " + count;
+
+        name += " " + count;
+
         if (ringer > 2)
         {
             ringer = 0;
@@ -42,25 +46,25 @@ public class Fractal : MonoBehaviour
 
         if (ringer == 0)
         {
-            spawnPos = Vector3.zero + spawnDir * radius;
+            spawnPos = pivot + spawnDir * radius;
         }
         else if (ringer == 1)
         {
-            spawnPos = Vector3.zero + spawnDir * (radius + 1);
+            spawnPos = pivot + spawnDir * (radius + 1);
         }
         else if (ringer == 2)
         {
-            spawnPos = Vector3.zero + spawnDir * (radius + 2);
+            spawnPos = pivot + spawnDir * (radius + 2);
         }
         else
         {
-            spawnPos = Vector3.zero + spawnDir * radius;
+            spawnPos = pivot + spawnDir * radius;
         }
 
         ringer += 1;
         if (count < objects)
         {
-            Fractal child = Instantiate(this, spawnPos, Quaternion.identity);
+            FractalRing child = Instantiate(this, spawnPos, Quaternion.identity);
             child.transform.SetParent(transform, false);
             child.transform.SetPositionAndRotation(spawnPos, Quaternion.identity);
         }
@@ -72,15 +76,15 @@ public class Fractal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (this.name == "Fractal 0")
+        if (this.name == first.name)
         {
             this.timer += Time.deltaTime;
-            this.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, Mathf.Abs(Mathf.Sin(((timer * 72) % 360) * (Mathf.PI / 180))), this.transform.position.z), Quaternion.identity);
+            this.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, pivot.y + Mathf.Abs(Mathf.Sin(((timer * 72) % 360) * (Mathf.PI / 180))), this.transform.position.z), Quaternion.identity);
         }
         else
         {
             this.timer += Time.deltaTime;
-            this.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, Mathf.Abs(Mathf.Sin((float)(((timer) * 72 % 360) * (Mathf.PI / 180)))), this.transform.position.z), Quaternion.identity);
+            this.transform.SetPositionAndRotation(new Vector3(this.transform.position.x, pivot.y + Mathf.Abs(Mathf.Sin((float)(((timer) * 72 % 360) * (Mathf.PI / 180)))), this.transform.position.z), Quaternion.identity);
         }
     }
 }
