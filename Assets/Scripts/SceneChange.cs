8b34bf3cd49785fa7e10e1725e54e4e2d5224ee6
Daniel <danielrios3000@gmail.com>
2022-02-11 22:37:15 -0500

    Fixed it so scenechanger won't teleport you to the same scene

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -6,15 +6,26 @@ using UnityEngine.SceneManagement;
 public class SceneChange : MonoBehaviour
 {
     public int whoScene = 0;
+    int notThisScene;
+    public void start()
+    {
+        whoScene = Random.Range(1, SceneManager.sceneCount);
+        if (SceneManager.GetSceneByBuildIndex(whoScene).name == SceneManager.GetActiveScene().name) {
+            notThisScene = whoScene;
+        }
+        while (whoScene == notThisScene) {
+            whoScene = Random.Range(1, SceneManager.sceneCount);
+        }
+    }
     void OnTriggerEnter(Collider other)
     {
         if (!this.tag.Equals("backToHub"))
         {
-            SceneManager.LoadScene(Random.Range(1, SceneManager.sceneCount));
+            SceneManager.LoadScene(whoScene);
         }
         else
         {
-            SceneManager.LoadScene(whoScene);
+            SceneManager.LoadScene(0);
         }
     }
 }
