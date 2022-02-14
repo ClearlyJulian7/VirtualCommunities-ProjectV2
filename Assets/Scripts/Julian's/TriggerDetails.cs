80d85be2d2be561f0688d4d7bf5e9c2c1b52624c
Julian P <91094720+ClearlyJulian7@users.noreply.github.com>
2022-02-13 22:35:18 -0500

    page flip

--- Assets/Scripts/Julian's/TriggerDetails.cs
+++ Assets/Scripts/Julian's/TriggerDetails.cs
@@ -7,12 +7,15 @@ public class TriggerDetails : MonoBehaviour
     public GameObject Artinfo;
     public GameObject ArtinfoQ;
 
+    public AudioSource flip;
+
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             Artinfo.SetActive(true);
             ArtinfoQ.SetActive(false);
+            flip.Play();
         }
         
     }
